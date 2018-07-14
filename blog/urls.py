8cc1fb9cd6e11b41231e7eb354fee0d864a2e61b
cemdrk <cemdurakgm@gmail.com>
2018-07-14 23:57:16 +0300

    added login logout

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.index),
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('post/<slug>', views.post_details, name= 'post_details'),
 ]
