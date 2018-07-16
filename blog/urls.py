e663743e98a8a05c1f02e9b49ce1a70c5445f547
cemdrk <cemdurakgm@gmail.com>
2018-07-16 14:14:21 +0300

    added create/delete post

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/', views.post_create, name='create'),
+    path('post/<id>/delete', views.post_delete, name='delete'),
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('post/<slug>', views.post_details, name= 'post_details'),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Post
+from .forms import PostForm
+
 
 def home(request):
     posts = Post.objects.order_by('created')
@@ -8,3 +11,20 @@ def home(request):
 def post_details(request, slug):
     post = get_object_or_404(Post, slug= slug)
     return render(request, 'blog/post_details.html', {'post': post})
+
+def post_create(request):
+    if request.user.is_authenticated:
+        form = PostForm(request.POST or None)
+        context = {'form': form}
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return HttpResponseRedirect('/')
+        return render(request, 'blog/post_create.html', context)
+    else:
+        return HttpResponseRedirect('/login')
+
+def post_delete(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    return HttpResponseRedirect('/')
